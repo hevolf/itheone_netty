0828516bf78ddaca68f1a6d55d9f5520c98fc4ea
itheone <caohaifengx@163.com>
2019-12-21 13:18:16 +0800

    first commit

---
+++ rpc-prepare/src/cn/enjoyedu/proxy/dynamic/JamesDyna.java
@@ -0,0 +1,30 @@
+package cn.enjoyedu.proxy.dynamic;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ *@author Mark老师   享学课堂 https://enjoy.ke.qq.com 
+ *
+ *类说明：代理类  动态代理
+ */
+public class JamesDyna implements InvocationHandler {
+
+    private Object receptionist;
+
+    // InvocationHandler里需要传入 被代理对象实例
+    public JamesDyna(Object receptionist) {
+        this.receptionist = receptionist;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args)
+            throws Throwable {
+        System.out.println("James find recp....");
+        Object result = method.invoke(receptionist,args);
+        System.out.println("half price...");
+        return result;
+    }
+
+
+}
---
+++ rpc-prepare/src/cn/enjoyedu/proxy/normal/Lison.java
@@ -0,0 +1,19 @@
+package cn.enjoyedu.proxy.normal;
+
+import cn.enjoyedu.proxy.IGetServant;
+import cn.enjoyedu.proxy.Receptionist;
+
+/**
+ * 直接调用
+ */
+public class Lison {
+
+    public static void main(String[] args) {
+        IGetServant getServant = new Receptionist();
+        getServant.choice("御姐范风格");
+
+//        IGetServant james = new James();
+//        james.choice("喜欢的样子");
+    }
+
+}
